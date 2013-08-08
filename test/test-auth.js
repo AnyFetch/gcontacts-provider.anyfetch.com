0166b383aa935138732667d2f6edd218e07f3da0
Neamar <neamar@neamar.fr>
2013-08-08 19:29:17 +0200

    Retrieve useful informations

--- test/test-auth.js
+++ test/test-auth.js
@@ -73,16 +73,49 @@ function getUserContacts(accessToken) {
     var feed = JSON.parse(body);
     var users = feed.feed.entry.map(function (c) {
       var r = {};
-      if(c['gd$name'] && ['gd$fullName']){
-        r.name = c['gd$name']['gd$fullName']['$t'];
+
+      if(c['gd$name']){
+        if(c['gd$name']['gd$fullName']) {
+          r.name = c['gd$name']['gd$fullName']['$t'];
+        }
+        if(c['gd$name']['gd$givenName']) {
+          r.given_name = c['gd$name']['gd$givenName']['$t'];
+        }
+        if(c['gd$name']['gd$familyName']) {
+          r.family_name = c['gd$name']['gd$familyName']['$t'];
+        }
       }
-      if (c['gd$email'] && c['gd$email'].length > 0) {
-        r.email = c['gd$email'][0]['address'];
+
+      if (c['gd$email']) {
+        r.emails = c['gd$email'].map(function(m) {
+          return m['address'];
+        });
+      }
+
+      if(c['gd$phoneNumber']) {
+        r.phones = c['gd$phoneNumber'].map(function(p) {
+          return p['$t'];
+        });
+      }
+
+      if(c['gd$im']) {
+        r.im = c['gd$im'].map(function(p) {
+          var hashIndex = p['protocol'].lastIndexOf('#')
+          return {
+            'address': p['address'],
+            'protocol': p['protocol'].substr(hashIndex+1)
+          };
+        });
+      }
+
+      if (c['gContact$birthday']) {
+        r.birthday = new Date(c['gContact$birthday']['when']);
       }
 
       return r;
     });
 
+    // Users is now full!
     console.log(users);
   });
 }
