1c03f8ac0ca477426b2bf62b57fd989f0605524a
Neamar <neamar@neamar.fr>
2013-08-09 16:36:06 +0200

    Test suite is running

--- test/retrieve.js
+++ test/retrieve.js
@@ -3,11 +3,11 @@
 var request = require('supertest')
 var should = require('should')
 var keys = require('../keys.js')
-var providerGoogleContact = require('../lib/provider-google-contact/index.js');
+var retrieve = require('../lib/provider-google-contact/helpers/retrieve.js');
 
 describe("Retrieve code", function () {
   it("should list contacts", function (done) {
-    providerGoogleContact.handlers.retrieve(keys.ACCESS_TOKEN, function(users) {
+    retrieve(keys.ACCESS_TOKEN, function(users) {
       should.exist(users[0]);
       users[0].should.have.property('name');
       done();
