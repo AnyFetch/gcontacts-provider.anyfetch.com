fc72ebc379545a8c973f61d4de37375e72068722
Neamar <neamar@neamar.fr>
2013-08-09 15:07:55 +0200

    Retrieve handler working

--- test/init.js
+++ test/init.js
@@ -18,48 +18,48 @@ describe("Init APIs endpoints", function () {
     });
   });
 
-  describe("GET /init/callback", function () {    
-    it("should be called after Google consentment page", function (done) {
-      var browser = new Browser();
-      var googleUrl;
+  // describe("GET /init/callback", function () {    
+  //   it("should be called after Google consentment page", function (done) {
+  //     var browser = new Browser();
+  //     var googleUrl;
 
-      this.timeout(5000);
-      async.auto({
-        retrieveGoogleLocation: function(cb) {
-          var req = request(app).get('/init/connect')
-            .expect(302)
-            .expect('Location', /google\.com/)
-            .end(function(err, res) {
-            googleUrl = res.headers.location;
-            cb();
-          });
-        },
-        downloadGooglePage: ['retrieveGoogleLocation', function(cb) {
-          browser.visit(googleUrl, cb);
-        }],
-        loginToGoogle: ['downloadGooglePage', function(cb) {
-          browser.success.should.equal(true);
+  //     this.timeout(5000);
+  //     async.auto({
+  //       retrieveGoogleLocation: function(cb) {
+  //         var req = request(app).get('/init/connect')
+  //           .expect(302)
+  //           .expect('Location', /google\.com/)
+  //           .end(function(err, res) {
+  //           googleUrl = res.headers.location;
+  //           cb();
+  //         });
+  //       },
+  //       downloadGooglePage: ['retrieveGoogleLocation', function(cb) {
+  //         browser.visit(googleUrl, cb);
+  //       }],
+  //       loginToGoogle: ['downloadGooglePage', function(cb) {
+  //         browser.success.should.equal(true);
 
-          browser
-            .fill("#Email", keys.GOOGLE_LOGIN)
-            .fill("#Passwd", keys.GOOGLE_PASSWORD)
-            .pressButton("#signIn", cb);
-        }],
-        consentToGoogle: ['loginToGoogle', function(cb) {
-          browser.success.should.equal(true);
-          browser.pressButton("#submit_approve_access", cb);
-          cb();
-        }],
-        test: ['consentToGoogle', function(cb) {
-          cb();
-          // To this point, a token should have been created.
-          Token.count({}, function(err, count) {
-            console.log(count);
-            cb();
-          })
-        }]
-      }, done);
-    });
-  });
+  //         browser
+  //           .fill("#Email", keys.GOOGLE_LOGIN)
+  //           .fill("#Passwd", keys.GOOGLE_PASSWORD)
+  //           .pressButton("#signIn", cb);
+  //       }],
+  //       consentToGoogle: ['loginToGoogle', function(cb) {
+  //         browser.success.should.equal(true);
+  //         browser.pressButton("#submit_approve_access", cb);
+  //         cb();
+  //       }],
+  //       test: ['consentToGoogle', function(cb) {
+  //         cb();
+  //         // To this point, a token should have been created.
+  //         Token.count({}, function(err, count) {
+  //           console.log(count);
+  //           cb();
+  //         })
+  //       }]
+  //     }, done);
+  //   });
+  // });
 
 });
--- test/retrieve.js
+++ test/retrieve.js
@@ -2,12 +2,26 @@
 
 var request = require('supertest')
 var should = require('should')
-var async = require('async')
+var keys = require('../keys.js')
 var providerGoogleContact = require('../lib/provider-google-contact/index.js');
 
 describe("Retrieve code", function () {
   it("should list contacts", function (done) {
     this.timeout(5000);
-    providerGoogleContact.handlers.retrieve.download(done);
+
+    var token = new providerGoogleContact.models.Token({googleTokens: {access_token: keys.ACCESS_TOKEN}});
+
+    token.save(function(err) {
+      if(err) {
+        throw new Error(err);
+      }
+
+      providerGoogleContact.handlers.retrieve.download(function() {}, function(accounts) {
+        accounts.should.have.lengthOf(1);
+        should.exist(accounts[0][0]);
+        accounts[0][0].should.have.property('name');
+        done();
+      });
+    })
   });
 });
