c28c8a9ff2609675d36edc34ff96fb35a726e80b
Neamar <neamar@neamar.fr>
2013-08-14 12:44:53 +0200

    Add lastAccess

--- test/retrieve.js
+++ test/retrieve.js
@@ -7,7 +7,7 @@ var retrieve = require('../lib/provider-google-contact/helpers/retrieve.js');
 
 describe("Retrieve code", function () {
   it("should list contacts", function (done) {
-    retrieve(keys.GOOGLE_TOKENS, function(users) {
+    retrieve(keys.GOOGLE_TOKENS, new Date(1970, 0, 1), function(users) {
       should.exist(users[0]);
       users[0].should.have.property('name');
       done();
