4aaa27773c47256cc495082847060c786d8d5dc7
Neamar <neamar@neamar.fr>
2013-08-22 16:31:46 +0200

    Handle deleted contacts

--- test/retrieve.js
+++ test/retrieve.js
@@ -5,7 +5,7 @@ var config = require('../config/configuration.js');
 var retrieve = require('../lib/provider-google-contact/helpers/retrieve.js');
 
 describe("Retrieve code", function () {
-  it("azer should list contacts", function (done) {
+  it("should list contacts", function (done) {
     retrieve(config.test_refresh_token, new Date(1970, 0, 1), function(users) {
       should.exist(users[0]);
       users[0].should.have.property('name');
@@ -14,7 +14,7 @@ describe("Retrieve code", function () {
   });
 
   it("should list contacts modified after specified date", function (done) {
-    retrieve(config.test_refresh_token, new Date(2020, 0, 1), function(users) {
+    retrieve(config.test_refresh_token, new Date(2020, 7, 22), function(users) {
       users.should.have.lengthOf(0);
       done();
     });
