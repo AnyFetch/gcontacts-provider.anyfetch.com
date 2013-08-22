be9d6850a0591c1c7d463f2929aa5397a6cdf674
Neamar <neamar@neamar.fr>
2013-08-22 12:37:03 +0200

    Compute more properties from contact: url

--- test/retrieve.js
+++ test/retrieve.js
@@ -5,7 +5,7 @@ var config = require('../config/configuration.js');
 var retrieve = require('../lib/provider-google-contact/helpers/retrieve.js');
 
 describe("Retrieve code", function () {
-  it("should list contacts", function (done) {
+  it("azer should list contacts", function (done) {
     retrieve(config.test_refresh_token, new Date(1970, 0, 1), function(users) {
       should.exist(users[0]);
       users[0].should.have.property('name');
