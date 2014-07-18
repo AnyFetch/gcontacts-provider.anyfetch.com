162d85b440737bda862e29da20292cd0c74c3cce
buathi_q <buathi_q@epitech.eu>
2014-07-18 16:29:57 +0200

    Refresh tokens at beginning

--- test/retrieve.js
+++ test/retrieve.js
@@ -1,35 +1,51 @@
 'use strict';
 
 var should = require('should');
+var googleapis = require('googleapis');
+var OAuth2Client = googleapis.OAuth2Client;
 var config = require('../config/configuration.js');
 var retrieve = require('../lib/helpers/retrieve.js');
 
 describe("Retrieve code", function () {
   it("should list contacts", function (done) {
-    retrieve(config.testRefreshToken, new Date(1970, 0, 1), function(err, contacts) {
+    var oauth2Client = new OAuth2Client(config.googleId, config.googleSecret, config.providerUrl + "/init/callback");
+    oauth2Client.refreshToken_(config.testRefreshToken, function(err, tokens) {
       if(err) {
-        throw err;
+        return done(err);
       }
 
-      should.exist(contacts[0]);
-      contacts[0].should.have.property('id', 'http://www.google.com/m8/feeds/contacts/test.cluestr%40gmail.com/base/587c929509de3a0a');
-      contacts[0].should.have.property('name', 'Matthieu Bacconnier');
-      contacts[0].should.have.property('given_name', 'Matthieu');
-      contacts[0].should.have.property('family_name', 'Bacconnier');
-      contacts[0].should.have.property('address');
+      retrieve(tokens.access_token, new Date(1970, 0, 1), function(err, contacts) {
+        if(err) {
+          throw err;
+        }
 
-      done();
+        should.exist(contacts[0]);
+        contacts[0].should.have.property('id', 'http://www.google.com/m8/feeds/contacts/test.cluestr%40gmail.com/base/587c929509de3a0a');
+        contacts[0].should.have.property('name', 'Matthieu Bacconnier');
+        contacts[0].should.have.property('given_name', 'Matthieu');
+        contacts[0].should.have.property('family_name', 'Bacconnier');
+        contacts[0].should.have.property('address');
+
+        done();
+      });
     });
   });
 
   it("should list contacts modified after specified date", function (done) {
-    retrieve(config.testRefreshToken, new Date(2020, 7, 22), function(err, contacts) {
+    var oauth2Client = new OAuth2Client(config.googleId, config.googleSecret, config.providerUrl + "/init/callback");
+    oauth2Client.refreshToken_(config.testRefreshToken, function(err, tokens) {
       if(err) {
-        throw err;
+        return done(err);
       }
 
-      contacts.should.have.lengthOf(0);
-      done();
+      retrieve(tokens.access_token, new Date(2020, 7, 22), function(err, contacts) {
+        if(err) {
+          throw err;
+        }
+
+        contacts.should.have.lengthOf(0);
+        done();
+      });
     });
   });
 });
