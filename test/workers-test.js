65693b701962c476c76be653265006dedc84e7f2
buathi_q <buathi_q@epitech.eu>
2014-11-20 15:09:51 +0100

    Update tests

---
+++ test/workers-test.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('should');
+var sinon = require('sinon');
+
+var workers = require('../lib/workers.js');
+
+module.exports.addition = function(job, cb) {
+  var spyPost = sinon.spy(job.anyfetchClient, "postDocument");
+
+  try {
+    job.task.should.have.property('url');
+    job.task.should.have.property('id');
+  } catch(e) {
+    return cb(e);
+  }
+
+  workers.addition(job, function(err) {
+    try {
+      spyPost.callCount.should.eql(1);
+    } catch(e) {
+      return cb(e);
+    }
+
+    cb(err);
+  });
+};
+
+module.exports.deletion = workers.deletion;
