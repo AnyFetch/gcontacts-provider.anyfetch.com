f753d163683cfbbde2ad6170c36dbfbdba9f7d9c
antonhansel <antoninribeaud@gmail.com>
2015-02-10 11:14:24 +0100

    Checking the id property of the first item

--- test/retrieve.js
+++ test/retrieve.js
@@ -39,6 +39,7 @@ describe("Retrieve contacts", function() {
   it('should list all contacts', function(done) {
     update(fakeServiceData, new Date(1970, 1, 1), fakeQueue, function(err) {
       connectionsPushed.length.should.be.above(0);
+      connectionsPushed[0].should.have.property('id');
       done(err);
     });
   });
